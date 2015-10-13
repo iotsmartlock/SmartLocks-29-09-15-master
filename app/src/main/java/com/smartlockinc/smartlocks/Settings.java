73cc0d9b1fc793795c2832c3c82fde08d5da7646
iotsmartlock <anant2.1317@gmail.com>
2015-10-13 15:40:10 +0530

    Initial commit

---
+++ app/src/main/java/com/smartlockinc/smartlocks/Settings.java
@@ -0,0 +1,51 @@
+package com.smartlockinc.smartlocks;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by anant on 09/10/15.
+ */
+public class Settings extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+
+
+
+
+        // Set up the drawer.
+
+
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
